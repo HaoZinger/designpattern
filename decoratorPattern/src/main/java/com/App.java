e1be7529a5eb5fab73873a1ffbe190caa15c1c2c
haozhigang <haozhigang@thunisoft.com>
2019-04-13 15:25:56 +0800

    装饰者模式

--- decoratorPattern/src/main/java/com/App.java
+++ decoratorPattern/src/main/java/com/App.java
@@ -11,15 +11,17 @@ public class App {
         originMethod();
         System.out.println("============");
         decoratorMethod();
-
+        System.out.println("************");
+        SichuanFood sichuanFood = new SichuanFood(new WestFood());
+        System.out.println("getSichuanFood-->\tspecies:" + sichuanFood.species() + "\tprice:" + sichuanFood.price()+"\tappraise: "+((ChineseFood) sichuanFood).appraise());
 
     }
 
     private static void decoratorMethod() {
         Food chineseFood = new ChineseFood(new SichuanFood(new WestFood()));
         Food dongbeiFood = new ChineseFood(new DongbeiFood(new WestFood()));
-        System.out.println("getChineseFood-->\tspecies:" + chineseFood.species() + "\tprice:" + chineseFood.price());
-        System.out.println("getDongbeiFood-->\tspecies:" + dongbeiFood.species() + "\tprice:" + dongbeiFood.price());
+        System.out.println("getChineseFood-->\tspecies:" + chineseFood.species() + "\tprice:" + chineseFood.price()+"\tappraise"+((ChineseFood) chineseFood).appraise());
+        System.out.println("getDongbeiFood-->\tspecies:" + dongbeiFood.species() + "\tprice:" + dongbeiFood.price()+"\tappraise"+((ChineseFood) dongbeiFood).appraise());
 
     }
 
